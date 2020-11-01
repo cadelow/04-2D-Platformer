6f40d4e7a1563355cc7fca1f01bfcf848bfad01a
cadelow <70243463+cadelow@users.noreply.github.com>
2020-11-01 00:11:38 -0400

    music added

--- Levels/Portal.gd
+++ Levels/Portal.gd
@@ -7,5 +7,7 @@ func _ready():
 
 func _on_Portal_body_entered(body):
 	if body.name == "Player":
+		Global.score = 0
+		Global.lives = 5
 		Global.level = 2
 		get_tree().change_scene("res://Levels/Level2.tscn")
